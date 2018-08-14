1acf10e36aaa8335a8deb4222cb2adb8feee5cbe
Veronica Bustabad <vbustabad@gmail.com>
2018-08-14 17:56:34 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
-  has_many :users, through: :comments
+  belongs_to :user
   accepts_nested_attributes_for :categories
 
   def categories_attributes=(category_attributes)
