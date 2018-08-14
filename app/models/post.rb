c51418bc0273d19c4cc51ccd22004c167b8d7470
Veronica Bustabad <vbustabad@gmail.com>
2018-08-14 16:06:48 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
-  accepts_nested_attributes_for :comments
-
+  accepts_nested_attributes_for :comments, reject_if: => :all_blank
+  
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
